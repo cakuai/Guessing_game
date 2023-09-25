0600ac179a671a219b54f1f407f9d36fe07faf4a
Cathy <kuai@CathydeMacBook-Pro.local>
2023-09-26 00:09:35 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+let input = prompt("Have a Guess! (Type 'q' to quit) ")
+let correctAns = Math.floor(Math.random()*10)+1
+let ct = 1;
+
+
+
+
+while (input != correctAns) {
+
+    if (input == "q") break;
+    input = Number(input);
+
+    if (input > correctAns){
+        input = prompt("Have a Guess again! (LOWER)");
+        ct++ ;
+    }
+    else if (input < correctAns){
+        input = prompt("Have a Guess again! (HIGHER)");
+        ct++ ;
+    }
+    else {
+        input = prompt("Please Enter a Valid Guess... or type 'q' to quit ");
+    }
+
+}
+
+if (input=="q") {
+    console.log("YOU QUIT!!!")
+}else {
+console.log(`CORRECT!!! It took you ${ct} guesses.`)
+console.log(correctAns)
+}
